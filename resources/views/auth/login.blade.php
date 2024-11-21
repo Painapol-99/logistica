7d0276adfb5779b2d48dd2eeb3f207432efe462a
Ismael Piña <ismael199904@gmail.com>
2024-11-21 16:59:01 +0100

    Bueno jeje esta el login y el registro enlazados con un poco de html y van JEJEJE

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('inicio') }}">
+    <form method="POST" action="{{ route('login') }}">
         @csrf
 
         <!-- Email Address -->
@@ -44,4 +44,10 @@
             </x-primary-button>
         </div>
     </form>
+
+    <div class="flex items-center justify-end mt-4">
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+            {{ __('Register') }}
+        </a>
+    </div>
 </x-guest-layout>
